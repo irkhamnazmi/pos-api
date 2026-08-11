19a15271f9610f659514dbab22d201d299847c21
asus <irkham.nazmi@gmail.com>
2026-08-11 17:02:38 +0700

    first commit

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\V1\ProductCategoryController;
+use App\Http\Controllers\Api\V1\ProductCategoryImageController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('v1')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/me', [AuthController::class, 'me']);
+        Route::post('/logout', [AuthController::class, 'logout']);
+
+
+        Route::get('product-categories/options', [ProductCategoryController::class, 'options']);
+        Route::post('product-categories/{id}/image', [ProductCategoryImageController::class, 'store']);
+        Route::apiResource('product-categories', ProductCategoryController::class);
+    });
+});
